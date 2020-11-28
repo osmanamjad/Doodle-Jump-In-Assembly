ba27690ffe1a8a807c57d1ded38987c405fdd3f4
Osman Amjad <m.osman.amjad@gmail.com>
2020-11-27 23:45:36 -0500

    change some registers to saved. implement jumping up and down

--- doodlejump.s
+++ doodlejump.s
@@ -10,72 +10,101 @@
 .data
 	displayAddress:	.word	0x10008000
 .text
-	lw $t0, displayAddress	# $t0 stores the base address for display
-	li $t1, 0xffd700	# $t1 stores the golden colour code
-	li $t2, 0x00ff00	# $t2 stores the green colour code
-	li $t3, 0x000000	# $t3 stores the black colour code
-	li $t4, 0xb0e0e6       # $t4 stores the aqua colour code
+	lw $s0, displayAddress	# $s0 stores the base address for display
+	li $s1, 0xffd700	# $s1 stores the golden colour code
+	li $s2, 0x00ff00	# $s2 stores the green colour code
+	li $s3, 0x000000	# $s3 stores the black colour code
+	li $s4, 0xb0e0e6       # $s4 stores the aqua colour code
 	
-	li $t5, 4096		# total size of board and the last unit of the display
-	add $t5, $t5, $t0	# set t5 to base address + 4096 offset
-	li $t6, 4096
+main:
+	jal makeBoardSetup
 
+	jal makeBoardBlue
+	
+	jal jumpUpSetup
+
+makeBoardSetup:
+	li $t5, 4096		# total size of board 
+	add $t5, $t5, $s0	# set t5 to base address + 4096 offset
+	li $t6, 4096		# to keep track of how many pixels we've coloured
+	
+	jr $ra
+	
 makeBoardBlue:
 	subi $t5, $t5, 4
 	subi $t6, $t6, 4
-	sw $t4, ($t5) # paint each unit
+	sw $s4, ($t5) # paint each unit
 	
-	beqz $t6, next1
+	beqz $t6, returnUp
 	j makeBoardBlue
+	
+makeCharacter: 
+	lw $t3, 0($sp) # load value at top of stack
+	addi $sp, $sp, 4 # decrease size of stack
+	
+	add $t7, $t3, $s0 #store the address we want the character to start from
+	
+	sw $s1, ($t7) #put golden in pixel in row 1
+	sw $s1, -4($t7) #put golden in pixel in row 1
+	sw $s1, -8($t7) #put golden in pixel in row 1
+	sw $s1, -12($t7) #put golden in pixel in row 1
+	sw $s1, -16($t7) #put golden in pixel in row 1
+	sw $s1, -128($t7) #put golden in pixel in row 2
+	sw $s1, -132($t7) #put golden in pixel in row 2
+	sw $s3, -136($t7) #put black in pixel in row 2
+	sw $s1, -140($t7) #put golden in pixel in row 2
+	sw $s1, -144($t7) #put golden in pixel in row 2
+	sw $s1, -260($t7) #put golden in pixel in row 3
+	sw $s1, -264($t7) #put golden in pixel in row 3
+	sw $s1, -268($t7) #put golden in pixel in row 3
+	
+	jr $ra
 
-next1:
-	li $t5 4028
-jump:
-	beqz, $t5, CentralProcessing #infinite loop
+returnUp:
+	jr $ra
+
+jumpUpSetup:
+	li $t4 4036
+	j jumpUp
+jumpUp:
+	bltz, $t4, jumpDownSetup 
+	
+	jal makeBoardSetup
+	jal makeBoardBlue
 	
 	addi $sp, $sp, -4 # put space on stack for start value
-	sw $t5, 0($sp) # load the value into the allocated space
+	sw $t4, 0($sp) # load the value into the allocated space
 	jal makeCharacter
 	
-	addi $t5, $t5, -256
+	addi $t4, $t4, -256
 	
 	li $v0, 32
 	li $a0, 100
 	syscall
 	
-	#jal makeBoardBlue
-	
-	j jump
-	
-	
-	
-makeCharacter: 
-	lw $t6, 0($sp) # load value at top of stack
-	addi $sp, $sp, 4 # decrease size of stack
-	
-	add $t7, $t6, $t0
-	
-	sw $t1, ($t7) #put golden in pixel in row 1
-	sw $t1, -4($t7) #put golden in pixel in row 1
-	sw $t1, -8($t7) #put golden in pixel in row 1
-	sw $t1, -12($t7) #put golden in pixel in row 1
-	sw $t1, -16($t7) #put golden in pixel in row 1
-	sw $t1, -128($t7) #put golden in pixel in row 2
-	sw $t1, -132($t7) #put golden in pixel in row 2
-	sw $t3, -136($t7) #put black in pixel in row 2
-	sw $t1, -140($t7) #put golden in pixel in row 2
-	sw $t1, -144($t7) #put golden in pixel in row 2
-	sw $t1, -260($t7) #put golden in pixel in row 3
-	sw $t1, -264($t7) #put golden in pixel in row 3
-	sw $t1, -268($t7) #put golden in pixel in row 3
-	
-	jr $ra
-	
-	
-	
+	j jumpUp
 
+jumpDownSetup:
+	li $t4 4036
+	li $t3 68
+	j jumpDown
+jumpDown:
+	bgt, $t3, $t4, jumpUpSetup 
 	
+	jal makeBoardSetup
+	jal makeBoardBlue
 	
+	addi $sp, $sp, -4 # put space on stack for start value
+	sw $t3, 0($sp) # load the value into the allocated space
+	jal makeCharacter
+	
+	addi $t3, $t3, 256
+	
+	li $v0, 32
+	li $a0, 100
+	syscall
+	
+	j jumpDown
 	
 	
 	
