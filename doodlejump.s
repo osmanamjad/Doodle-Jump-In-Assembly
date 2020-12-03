92fa063c52c0ae795edc0cb5c5dbd1342f946304
Osman Amjad <m.osman.amjad@gmail.com>
2020-12-03 14:09:05 -0500

    add functionality for working version of game!

--- doodlejump.s
+++ doodlejump.s
@@ -35,7 +35,7 @@
 	displayAddress:	.word	0x10008000
 	charStartAddress: .word 0
 	charBottomAddress: .word 0 # store the address of bottom address doodle should jump till
-	ledge1StartAddress: .word 4016
+	ledge1StartAddress: .word 0
 	ledge2StartAddress: .word 0
 	ledge3StartAddress: .word 0
 	
@@ -109,10 +109,11 @@ makeCharacter:
 jumpUpSetup:
 	lw $t4, charStartAddress
 	addi $t4, $t4, -128 # subtract 256 to ensure its a valid address
+	addi $t3, $t4, -2400
 	sw $t4, charStartAddress
 	j jumpUp
 jumpUp:
-	blt, $t4, $s0, jumpDownSetup # branch if t4 goes below the minimum valid address
+	blt, $t4, $t3, jumpDownSetup # branch if t4 goes below max jumping capacity
 	
 	jal makeBoardSetup
 	jal makeBoardBlue
@@ -159,8 +160,9 @@ jumpDown:
 	jal makeCharacter
 	jal checkLanding
 afterCheckLanding:
-	lw $t3, charBottomAddress # load charBottom address into t2 in case it changed
+	lw $t3, charBottomAddress # load charBottom address into t3 in case it changed
 	
+	lw $t4, charStartAddress # load back into t4 in case it changed
 	addi $t4, $t4, 128
 	sw $t4, charStartAddress
 	
@@ -182,16 +184,72 @@ checkInput2:
 
 	j jumpDown
 	
+ledge1Setup:
+	lw $t0, ledge1StartAddress
+	bgt $t0, $s7, makeLedge1 #if its start address is more than max display address, remake it
+	jr $ra
+makeLedge1:	
+	li $v0, 42 # prepare syscall to produce random int
+	li $a0, 0 
+	li $a1, 24 # set max value of random int
+	syscall
+	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
+	addi $a0, $a0, 2800 #add 3000 so the ledge is near bottom of screen
+	add $t7, $s0, $a0 #add display address + random int to get new start 
+	sw $t7, ledge1StartAddress
+	
+	jr $ra
+	
+ledge2Setup:
+	lw $t0, ledge2StartAddress
+	bgt $t0, $s7, makeLedge2 #if its start address is more than max display address, remake it
+	
+	jr $ra
+makeLedge2:	
+	li $v0, 42 # prepare syscall to produce random int
+	li $a0, 0 
+	li $a1, 24 # set max value of random int
+	syscall
+	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
+	addi $a0, $a0, 1000 #add 1000 so the ledge isnt too high on the screen
+	add $t7, $s0, $a0 #add display address + random int to get new start 
+	sw $t7, ledge2StartAddress
+	
+	jr $ra
+	
+ledge3Setup:
+	lw $t0, ledge3StartAddress
+	bgt $t0, $s7, makeLedge3 #if its start address is more than max display address, remake it
+	jr $ra
+makeLedge3:	
+	li $v0, 42 # prepare syscall to produce random int
+	li $a0, 0 
+	li $a1, 24 # set max value of random int to 700
+	syscall
+	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
+	addi $a0, $a0, 2000 #add 2000 so the ledge isnt too high on the screen
+	add $t7, $s0, $a0 #add display address + random int to get new start 
+	sw $t7, ledge3StartAddress
+	
+	jr $ra
+		
 makeLedgesSetup:
-	lw $t7, ledge1StartAddress # load 4036 into t7
-	add $t7, $s0, $t7 #add display address + 4036 
-	sw $t7, ledge1StartAddress 
+	li $v0, 42 # prepare syscall to produce random int
+	li $a0, 0 
+	li $a1, 24 # set max value of random int
+	syscall
+	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
+	addi $a0, $a0, 3000 #add 3000 so the ledge is near bottom of screen
+	add $t7, $s0, $a0 #add display address + random int to get new start 
+	sw $t7, ledge1StartAddress
+	sw $t7, charBottomAddress
+	addi $t7, $t7, -128 #use this value for charStartAddress
+	sw $t7, charStartAddress
 	
 	li $v0, 42 # prepare syscall to produce random int
 	li $a0, 0 
-	li $a1, 700 # set max value of random int
+	li $a1, 24 # set max value of random int
 	syscall
-	
 	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
 	addi $a0, $a0, 1000 #add 1000 so the ledge isnt too high on the screen
 	add $t7, $s0, $a0 #add display address + random int to get new start 
@@ -199,11 +257,10 @@ makeLedgesSetup:
 	
 	li $v0, 42 # prepare syscall to produce random int
 	li $a0, 0 
-	li $a1, 700 # set max value of random int to 700
+	li $a1, 24 # set max value of random int to 700
 	syscall
-	
 	sll $a0, $a0, 2 #multiply the random int by 4 to ensure its a multiple of 4 to use with displayAddress
-	addi $a0, $a0, 1000 #add 1000 so the ledge isnt too high on the screen
+	addi $a0, $a0, 2000 #add 2000 so the ledge isnt too high on the screen
 	add $t7, $s0, $a0 #add display address + random int to get new start 
 	sw $t7, ledge3StartAddress
 	
@@ -268,79 +325,78 @@ checkLanding:
 	lw $t3, charStartAddress
 	
 	addi $t7, $t3, 152
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	addi $t7, $t7, -4
-	beq $t7, $t0, ledge1NewBase
-	beq $t7, $t1, ledge2NewBase
-	beq $t7, $t2, ledge3NewBase
+	beq $t7, $t0, ledgeNewBase
+	beq $t7, $t1, ledgeNewBase
+	beq $t7, $t2, ledgeNewBase
 	
 	sw $s5, charBottomAddress #if we don't land on a ledge, then make bottom back to default
 	j afterCheckLanding #return to jumpDown
 
-ledge3NewBase:
+ledgeNewBase:
 	sw $t3, charBottomAddress # since we've landed, make current charStartAddress the new bottom
 	
 	addi $t3, $t3, 128
 	sw $t3, charStartAddress
-	#bgt $t3, $s6, setBottomBack
+	bge $t3, $s6, doneScrolling #s6 is the max address in first row. $t3 cant go below first row
+	
+	addi $t0, $t0, 128
+	sw $t0, ledge1StartAddress
 	
 	addi $t1, $t1, 128
 	sw $t1, ledge2StartAddress
-	#bgt $t7, $s6, randomizeLedge3 # branch if t7 exceeds max display address
-	
-	#bgt $t6, $s6, Y
+
 	addi $t2, $t2, 128
 	sw $t2, ledge3StartAddress
-	
-	#blt $t5, $s0, Z
 	
 	jal makeBoardSetup
 	jal makeBoardBlue
@@ -349,21 +405,27 @@ ledge3NewBase:
 	jal makeCharacter
 	
 	li $v0, 32
-	li $a0, 50
+	li $a0, 20
 	syscall
 	
-	j ledge3NewBase
-setBottomBack:
+	j ledgeNewBase
+doneScrolling:
+	addi $t3, $t3, -128
+	sw $t3, charStartAddress
+	
+	jal ledge1Setup
+	jal ledge2Setup
+	jal ledge3Setup
 	
 	j afterCheckLanding #return to jumpDown
 	
-ledge2NewBase:
-	sw $t3, charBottomAddress #since we've landed, make current charStartAddress the new bottom
-	j afterCheckLanding #return to jumpDown
+#ledge2NewBase:
+#	sw $t3, charBottomAddress #since we've landed, make current charStartAddress the new bottom
+#	j afterCheckLanding #return to jumpDown
 
-ledge1NewBase:
-	sw $t3, charBottomAddress #since we've landed, make current charStartAddress the new bottom
-	j afterCheckLanding #return to jumpDown
+#ledge1NewBase:
+#	sw $t3, charBottomAddress #since we've landed, make current charStartAddress the new bottom
+#	j afterCheckLanding #return to jumpDown
 		
 		
 CentralProcessing:
